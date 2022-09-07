e88179b120d6075d275c1dd4bd63396e7250f60e
Hieu Vo Trong <vtronghieu@tma.com.vn>
2022-09-07 17:31:03 +0700

    None

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class test {
+    public static void main(String[] args) {
+        testModel testModel = new testModel();
+        testModel.clearTest();
+        testModel.showTest();
+        //testModel.addTest();
+        //testModel.deleteTest();
+    }
+}
